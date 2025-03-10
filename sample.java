278cae3f0151b5a6aabd4b86170e00b8cbe1c74b
IncharaIse <130211628+IncharaIse@users.noreply.github.com>
2025-03-10 09:51:35 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        // Create a scanner object to take input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is odd or even
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
