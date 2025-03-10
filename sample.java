ff0dfdd6b7a7607a736bfc39821ab810888c0589
IncharaIse <130211628+IncharaIse@users.noreply.github.com>
2025-03-10 10:02:23 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class OddEven {
+public class sample {
     public static void main(String[] args) {
         // Create a scanner object to take input from the user
         Scanner scanner = new Scanner(System.in);
